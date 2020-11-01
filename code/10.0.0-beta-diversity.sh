ee1f45f68b63e9fd24887d010f8c71ff9861aedd
Jincheng Wang <tornadow18@gmail.com>
2020-11-01 14:19:54 -0500

    1. Generate the rarefied feature table for the entire dataset at 5k 2. Update the script for generating beta diversity (bray, unifrac, unweighted unifrac and aitchison), the distance matrix it self has to be saved separately due to size.

--- code/10.0.0-beta-diversity.sh
+++ code/10.0.0-beta-diversity.sh
@@ -4,28 +4,28 @@ qiime diversity beta --i-table ../data/split-data/Baby-Forearm/rarefy-table.qza 
 
 qiime diversity beta --i-table ../data/split-data/Baby-Mouth/rarefy-table.qza --p-metric 'aitchison' --p-n-jobs 8 --output-dir ../data/beta-results/Baby-Mouth-aitchison/ &>BabyMouthAitchison.log &
 
-qiime diversity beta-phylogenetic --i-table ../data/split-data/Baby-Feces/rarefy-table.qza --i-phylogeny tree.qza --p-metric unweighted_unifrac --p-n-jobs 4 --output-dir ../data/beta-results/Baby-Feces-unifrac/ &>BabyFecesUnifrac.log &
+qiime diversity beta-phylogenetic --i-table ../data/split-data/Baby-Feces/rarefy-table.qza --i-phylogeny ../data/processed-data/tree.qza --p-metric unweighted_unifrac --p-n-jobs 4 --output-dir ../data/beta-results/Baby-Feces-unifrac/ &>BabyFecesUnifrac.log &
 
-qiime diversity beta-phylogenetic --i-table ../data/split-data/Baby-Forearm/rarefy-table.qza --i-phylogeny tree.qza --p-metric unweighted_unifrac --p-n-jobs 4 --output-dir ../data/beta-results/Baby-Forearm-unifrac/ &>BabyForearmUnifrac.log &
+qiime diversity beta-phylogenetic --i-table ../data/split-data/Baby-Forearm/rarefy-table.qza --i-phylogeny ../data/processed-data/tree.qza --p-metric unweighted_unifrac --p-n-jobs 4 --output-dir ../data/beta-results/Baby-Forearm-unifrac/ &>BabyForearmUnifrac.log &
 
-qiime diversity beta-phylogenetic --i-table ../data/split-data/Baby-Mouth/rarefy-table.qza --i-phylogeny tree.qza --p-metric unweighted_unifrac --p-n-jobs 4 --output-dir ../data/beta-results/Baby-Mouth-unifrac/ &>BabyMouthUnifrac.log &
+qiime diversity beta-phylogenetic --i-table ../data/split-data/Baby-Mouth/rarefy-table.qza --i-phylogeny ../data/processed-data/tree.qza --p-metric unweighted_unifrac --p-n-jobs 4 --output-dir ../data/beta-results/Baby-Mouth-unifrac/ &>BabyMouthUnifrac.log &
 
-qiime diversity beta-phylogenetic --i-table ../data/split-data/Baby-Feces/rarefy-table.qza --i-phylogeny tree.qza --p-metric weighted_unifrac --p-n-jobs 4 --output-dir ../data/beta-results/Baby-Feces-wunifrac/ &>BabyFecesWUnifrac.log &
+qiime diversity beta-phylogenetic --i-table ../data/split-data/Baby-Feces/rarefy-table.qza --i-phylogeny ../data/processed-data/tree.qza --p-metric weighted_unifrac --p-n-jobs 4 --output-dir ../data/beta-results/Baby-Feces-wunifrac/ &>BabyFecesWUnifrac.log &
 
-qiime diversity beta-phylogenetic --i-table ../data/split-data/Baby-Forearm/rarefy-table.qza --i-phylogeny tree.qza --p-metric weighted_unifrac --p-n-jobs 4 --output-dir ../data/beta-results/Baby-Forearm-wunifrac/ &>BabyForearmWUnifrac.log &
+qiime diversity beta-phylogenetic --i-table ../data/split-data/Baby-Forearm/rarefy-table.qza --i-phylogeny ../data/processed-data/tree.qza --p-metric weighted_unifrac --p-n-jobs 4 --output-dir ../data/beta-results/Baby-Forearm-wunifrac/ &>BabyForearmWUnifrac.log &
 
-qiime diversity beta-phylogenetic --i-table ../data/split-data/Baby-Mouth/rarefy-table.qza --i-phylogeny tree.qza --p-metric weighted_unifrac --p-n-jobs 4 --output-dir ../data/beta-results/Baby-Mouth-wunifrac/ &>BabyMouthWUnifrac.log &
+qiime diversity beta-phylogenetic --i-table ../data/split-data/Baby-Mouth/rarefy-table.qza --i-phylogeny ../data/processed-data/tree.qza --p-metric weighted_unifrac --p-n-jobs 4 --output-dir ../data/beta-results/Baby-Mouth-wunifrac/ &>BabyMouthWUnifrac.log &
 
 
  qiime feature-table rarefy --i-table ../data/processed-data/table.qza --p-sampling-depth 5000 --o-rarefied-table ../data/processed-data/rarefy-table-5k.qza &>Rarefy_processed_table.log &
 
-qiime diversity beta --i-table rarefy-table-5k.qza --p-metric 'aitchison' --p-n-jobs 24 --output-dir rarefied-aitchison/ &>RarefiedAitchison.log &
-qiime diversity beta --i-table rarefy-table-5k.qza --p-metric 'braycurtis' --p-n-jobs 8 --output-dir rarefied-bray/ &>Rarefiedbray.log &
-qiime diversity beta-phylogenetic --i-table rarefy-table-5k.qza --i-phylogeny tree.qza --p-metric unweighted_unifrac --p-n-jobs 12 --output-dir rarefied-unifrac/ &>RarefiedUnifrac.log &
-qiime diversity beta-phylogenetic --i-table rarefy-table-5k.qza --i-phylogeny tree.qza --p-metric weighted_unifrac --p-n-jobs 12 --output-dir rarefied-wunifrac/ &>RarefiedWUnifrac.log &
+qiime diversity beta --i-table ../data/processed-data/rarefy-table-5k.qza --p-metric 'aitchison' --p-n-jobs 24 --output-dir ../data/beta-results/rarefied-aitchison/ &>RarefiedAitchison.log &
+qiime diversity beta --i-table ../data/processed-data/rarefy-table-5k.qza --p-metric 'braycurtis' --p-n-jobs 8 --output-dir ../data/beta-results/rarefied-bray/ &>Rarefiedbray.log &
+qiime diversity beta-phylogenetic --i-table ../data/processed-data/rarefy-table-5k.qza --i-phylogeny ../data/processed-data/tree.qza --p-metric unweighted_unifrac --p-n-jobs 12 --output-dir ../data/beta-results/rarefied-unifrac/ &>RarefiedUnifrac.log &
+qiime diversity beta-phylogenetic --i-table ../data/processed-data/rarefy-table-5k.qza --i-phylogeny ../data/processed-data/tree.qza --p-metric weighted_unifrac --p-n-jobs 12 --output-dir ../data/beta-results/rarefied-wunifrac/ &>RarefiedWUnifrac.log &
 
 
-qiime diversity beta --i-table table.qza --p-metric aitchison --p-n-jobs 8 --output-dir original-aitchison/ &>OriginalAitchison.log &
-qiime diversity beta --i-table table.qza --p-metric braycurtis --p-n-jobs 8 --output-dir original-bray/ &>Originalbray.log &
-qiime diversity beta-phylogenetic --i-table table.qza --i-phylogeny tree.qza --p-metric unweighted_unifrac --p-n-jobs 8 --output-dir original-unifrac/ &>OriginalUnifrac.log &
-qiime diversity beta-phylogenetic --i-table table.qza --i-phylogeny tree.qza --p-metric weighted_unifrac --p-n-jobs 8 --output-dir original-wunifrac/ &>OriginalWUnifrac.log &
+qiime diversity beta --i-table ../data/processed-data/table.qza --p-metric aitchison --p-n-jobs 8 --output-dir ../data/beta-results/original-aitchison/ &>OriginalAitchison.log &
+qiime diversity beta --i-table ../data/processed-data/table.qza --p-metric braycurtis --p-n-jobs 8 --output-dir ../data/beta-results/original-bray/ &>Originalbray.log &
+qiime diversity beta-phylogenetic --i-table ../data/processed-data/table.qza --i-phylogeny ../data/processed-data/tree.qza --p-metric unweighted_unifrac --p-n-jobs 8 --output-dir ../data/beta-results/original-unifrac/ &>OriginalUnifrac.log &
+qiime diversity beta-phylogenetic --i-table ../data/processed-data/table.qza --i-phylogeny ../data/processed-data/tree.qza --p-metric weighted_unifrac --p-n-jobs 8 --output-dir ../data/beta-results/original-wunifrac/ &>OriginalWUnifrac.log &

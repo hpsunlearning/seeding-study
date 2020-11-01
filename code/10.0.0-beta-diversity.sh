1e9f8e4f8a6c9fdb2fb03fa4217c7cf0f2cf7db8
Jincheng Wang <tornadow18@gmail.com>
2020-10-31 23:14:32 -0400

    update codes to process the entire dataset

--- code/10.0.0-beta-diversity.sh
+++ code/10.0.0-beta-diversity.sh
@@ -15,3 +15,17 @@ qiime diversity beta-phylogenetic --i-table ../data/split-data/Baby-Feces/rarefy
 qiime diversity beta-phylogenetic --i-table ../data/split-data/Baby-Forearm/rarefy-table.qza --i-phylogeny tree.qza --p-metric weighted_unifrac --p-n-jobs 4 --output-dir ../data/beta-results/Baby-Forearm-wunifrac/ &>BabyForearmWUnifrac.log &
 
 qiime diversity beta-phylogenetic --i-table ../data/split-data/Baby-Mouth/rarefy-table.qza --i-phylogeny tree.qza --p-metric weighted_unifrac --p-n-jobs 4 --output-dir ../data/beta-results/Baby-Mouth-wunifrac/ &>BabyMouthWUnifrac.log &
+
+
+ qiime feature-table rarefy --i-table ../data/processed-data/table.qza --p-sampling-depth 5000 --o-rarefied-table ../data/processed-data/rarefy-table-5k.qza &>Rarefy_processed_table.log &
+
+qiime diversity beta --i-table rarefy-table-5k.qza --p-metric 'aitchison' --p-n-jobs 24 --output-dir rarefied-aitchison/ &>RarefiedAitchison.log &
+qiime diversity beta --i-table rarefy-table-5k.qza --p-metric 'braycurtis' --p-n-jobs 8 --output-dir rarefied-bray/ &>Rarefiedbray.log &
+qiime diversity beta-phylogenetic --i-table rarefy-table-5k.qza --i-phylogeny tree.qza --p-metric unweighted_unifrac --p-n-jobs 12 --output-dir rarefied-unifrac/ &>RarefiedUnifrac.log &
+qiime diversity beta-phylogenetic --i-table rarefy-table-5k.qza --i-phylogeny tree.qza --p-metric weighted_unifrac --p-n-jobs 12 --output-dir rarefied-wunifrac/ &>RarefiedWUnifrac.log &
+
+
+qiime diversity beta --i-table table.qza --p-metric aitchison --p-n-jobs 8 --output-dir original-aitchison/ &>OriginalAitchison.log &
+qiime diversity beta --i-table table.qza --p-metric braycurtis --p-n-jobs 8 --output-dir original-bray/ &>Originalbray.log &
+qiime diversity beta-phylogenetic --i-table table.qza --i-phylogeny tree.qza --p-metric unweighted_unifrac --p-n-jobs 8 --output-dir original-unifrac/ &>OriginalUnifrac.log &
+qiime diversity beta-phylogenetic --i-table table.qza --i-phylogeny tree.qza --p-metric weighted_unifrac --p-n-jobs 8 --output-dir original-wunifrac/ &>OriginalWUnifrac.log &
